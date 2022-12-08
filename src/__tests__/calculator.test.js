474061565683dca44aaf2d545b6258d52080051c
Tamunokuro <tamunokrobluejack@gmail.com>
2022-12-08 12:58:24 +0100

    Add Tests

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,16 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
-describe("Test for calculator component", () => {
-    it('it should render the number of buttons in the calculator', () => {
-        render(<Calculator />);
-        const buttonElements = screen.getAllByRole('button');
-        expect(buttonElements.length).toBe(19);
-      });
+describe('Test for calculator component', () => {
+  it('it should render the number of buttons in the calculator', () => {
+    render(<Calculator />);
+    const buttonElements = screen.getAllByRole('button');
+    expect(buttonElements.length).toBe(19);
+  });
 
-    it("Snapshot should match component render", () => {
-        const calculator = render(<Calculator />)
-        expect(calculator).toMatchSnapshot()
-    })
-})
-
+  it('Snapshot should match component render', () => {
+    const calculator = render(<Calculator />);
+    expect(calculator).toMatchSnapshot();
+  });
+});
