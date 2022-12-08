933b6db89f3226bc816e4df059e5957e3e8fe3e0
Tamunokuro <tamunokrobluejack@gmail.com>
2022-12-08 13:39:08 +0100

    Add tests

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
 describe('Test for calculator component', () => {
@@ -11,5 +11,19 @@ describe('Test for calculator component', () => {
   it('Snapshot should match component render', () => {
     const calculator = render(<Calculator />);
     expect(calculator).toMatchSnapshot();
+  });
+
+  it('it add 7 and 3 By click button', () => {
+    render(<Calculator />);
+    const getFirtBtn = screen.getByTestId('7');
+    const getSecondBtn = screen.getByTestId('3');
+    const getOpBtn = screen.getByTestId('+');
+    const getEqualBtn = screen.getByTestId('=');
+    const calScreen = screen.getByTestId('screen');
+    fireEvent.click(getFirtBtn, { target: { innerText: '7' } });
+    fireEvent.click(getOpBtn, { target: { innerText: '+' } });
+    fireEvent.click(getSecondBtn, { target: { innerText: '3' } });
+    fireEvent.click(getEqualBtn, { target: { innerText: '=' } });
+    expect(calScreen.textContent).toBe('10');
   });
 });
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,7 +12,7 @@ const Calculator = () => {
   });
 
   function handleButtonChange(e) {
-    const btnVal = e.target.name;
+    const btnVal = e.target.innerText;
     return buttonChange((obj) => calculate(obj, btnVal));
   }
 
@@ -25,33 +25,31 @@ const Calculator = () => {
           </Col>
           <Col xs={6}>
             <div className="calculator">
-              <span className="results">
+              <span data-testid="screen" className="results">
                 {obj.total}
-                {' '}
                 {obj.operation}
-                {' '}
                 {obj.next}
               </span>
 
-              <button type="button" className="numbers" name="AC" onClick={handleButtonChange}>AC</button>
-              <button type="button" className="numbers" name="+/-" onClick={handleButtonChange}>+/-</button>
-              <button type="button" className="numbers" name="%" onClick={handleButtonChange}>%</button>
-              <button type="button" className="operators" name="&divide;" onClick={handleButtonChange}>&divide;</button>
-              <button type="button" className="numbers" name="7" onClick={handleButtonChange}>7</button>
-              <button type="button" className="numbers" name="8" onClick={handleButtonChange}>8</button>
-              <button type="button" className="numbers" name="9" onClick={handleButtonChange}>9</button>
-              <button type="button" className="operators" name="x" onClick={handleButtonChange}>x</button>
-              <button type="button" className="numbers" name="4" onClick={handleButtonChange}>4</button>
-              <button type="button" className="numbers" name="5" onClick={handleButtonChange}>5</button>
-              <button type="button" className="numbers" name="6" onClick={handleButtonChange}>6</button>
-              <button type="button" className="operators" name="-" onClick={handleButtonChange}>-</button>
-              <button type="button" className="numbers" name="1" onClick={handleButtonChange}>1</button>
-              <button type="button" className="numbers" name="2" onClick={handleButtonChange}>2</button>
-              <button type="button" className="numbers" name="3" onClick={handleButtonChange}>3</button>
-              <button type="button" className="operators" name="+" onClick={handleButtonChange}>+</button>
-              <button type="button" className="numbers span-zero" name="0" onClick={handleButtonChange}>0</button>
-              <button type="button" className="numbers" name="." onClick={handleButtonChange}>.</button>
-              <button type="button" className="operators" name="=" onClick={handleButtonChange}>=</button>
+              <button type="button" className="numbers" data-testid="AC" onClick={handleButtonChange}>AC</button>
+              <button type="button" className="numbers" data-testid="+/-" onClick={handleButtonChange}>+/-</button>
+              <button type="button" className="numbers" data-testid="%" onClick={handleButtonChange}>%</button>
+              <button type="button" className="operators" data-testid="&divide;" onClick={handleButtonChange}>&divide;</button>
+              <button type="button" className="numbers" data-testid="7" onClick={handleButtonChange}>7</button>
+              <button type="button" className="numbers" data-testid="8" onClick={handleButtonChange}>8</button>
+              <button type="button" className="numbers" data-testid="9" onClick={handleButtonChange}>9</button>
+              <button type="button" className="operators" data-testid="x" onClick={handleButtonChange}>x</button>
+              <button type="button" className="numbers" data-testid="4" onClick={handleButtonChange}>4</button>
+              <button type="button" className="numbers" data-testid="5" onClick={handleButtonChange}>5</button>
+              <button type="button" className="numbers" data-testid="6" onClick={handleButtonChange}>6</button>
+              <button type="button" className="operators" data-testid="-" onClick={handleButtonChange}>-</button>
+              <button type="button" className="numbers" data-testid="1" onClick={handleButtonChange}>1</button>
+              <button type="button" className="numbers" data-testid="2" onClick={handleButtonChange}>2</button>
+              <button type="button" className="numbers" data-testid="3" onClick={handleButtonChange}>3</button>
+              <button type="button" className="operators" data-testid="+" onClick={handleButtonChange}>+</button>
+              <button type="button" className="numbers span-zero" data-testid="0" onClick={handleButtonChange}>0</button>
+              <button type="button" className="numbers" data-testid="." onClick={handleButtonChange}>.</button>
+              <button type="button" className="operators" data-testid="=" onClick={handleButtonChange}>=</button>
             </div>
           </Col>
         </Row>
